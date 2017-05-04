017967074e45e2465ea90e66c86120174819e9e2
Aravind <aravind@Aravind.local>
2017-05-04 23:34:13 +0530

    Extension added

---
+++ GradNext/Utilities/Extensions.swift
@@ -0,0 +1,23 @@
+//
+//  Extensions.swift
+//  GradNext
+//
+//  Created by Aravind on 5/4/17.
+//  Copyright © 2017 swathi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UITextField {
+    func setLeftPaddingPoints(_ amount:CGFloat){
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.leftView = paddingView
+        self.leftViewMode = .always
+    }
+    func setRightPaddingPoints(_ amount:CGFloat) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.rightView = paddingView
+        self.rightViewMode = .always
+    }
+}
