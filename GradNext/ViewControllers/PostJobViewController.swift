c06bbda0cd8f779b97845c0944e246b3b2d35f66
Aravind <aravind@Aravind.local>
2017-05-14 22:44:17 +0530

    changes

--- GradNext/ViewControllers/PostJobViewController.swift
+++ GradNext/ViewControllers/PostJobViewController.swift
@@ -1,5 +1,5 @@
 //
-//  CandidateFilterViewController.swift
+//  PostJobViewController.swift
 //  GradNext
 //
 //  Created by Aravind on 5/14/17.
@@ -8,68 +8,77 @@
 
 import UIKit
 
-class CandidateFilterViewController: UIViewController,UITextFieldDelegate {
-    @IBOutlet weak var txtKeywords: UITextField!
-    @IBOutlet weak var btnReset: UIButton!
-    @IBOutlet weak var btnFilter: UIButton!
-    @IBOutlet weak var txtSkills: UITextField!
-    
-    @IBOutlet weak var btnApply: UIButton!
+class PostJobViewController: UIViewController , UITextFieldDelegate,UITextViewDelegate{
+    @IBOutlet weak var txtYearOfGraduation: UITextField!
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var btnSubmit: UIButton!
+    @IBOutlet weak var btnJobType: UIButton!
+    @IBOutlet weak var btnUploadVideo: UIButton!
+    @IBOutlet weak var enableVideoQuestionSwitch: UISwitch!
+    @IBOutlet weak var txtAboutJob: UITextView!
+    @IBOutlet weak var txtJobTitle: UITextField!
     
     var activeField: UITextField?
-    @IBOutlet weak var scrollView: UIScrollView!
+    var activeTextView: UITextView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       
-        
+
         // Do any additional setup after loading the view.
-        
-    }
-    
-    override func viewDidLayoutSubviews()
-    {
-       customControls()
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-       
-       registerForKeyboardNotifications()
-    }
-    override func viewWillDisappear(_ animated: Bool) {
-        deregisterFromKeyboardNotifications()
-        
+        registerForKeyboardNotifications()
     }
 
-    func customControls() {
-        
-        Utilities.setTextFieldCornerRadius(forTextField: txtKeywords, withRadius: 3.0, withBorderColor:  UIColor.gray)
-        Utilities.setTextFieldCornerRadius(forTextField: txtSkills, withRadius: 3.0, withBorderColor:  UIColor.gray)
-        
-        Utilities.setButtonCornerRadius(button: btnFilter, withRadius: 3.0, withBorderColor: UIColor.gray)
-        
-        txtSkills.setLeftPaddingPoints(10)
-        txtKeywords.setLeftPaddingPoints(10)
-        
-        
-        
-        btnApply.layer.cornerRadius = 3.0
-        btnReset.layer.cornerRadius = 3.0
-        
-    }
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        deregisterFromKeyboardNotifications()
+    }
+    
+    @IBAction func skillsButtonClicked(_ sender: Any) {
+        
+    }
+    
     @IBAction func closeButtonClicked(_ sender: Any) {
-        self.presentingViewController?.dismiss(animated: true, completion: nil)
+           self.presentingViewController?.dismiss(animated: true, completion: nil)
     }
-  
-    @IBAction func applyButtonClicked(_ sender: Any) {
+    
+    @IBAction func submitButtonClicked(_ sender: Any) {
+        
     }
-   
-    @IBAction func resetButtonClicked(_ sender: Any) {
+    
+    
+    @IBAction func jobTypeButtonClicked(_ sender: Any) {
+    }
+    override func viewDidLayoutSubviews()
+    {
+        customControls()
+    }
+    func customControls() {
+        
+        Utilities.setTextFieldCornerRadius(forTextField: txtYearOfGraduation, withRadius: 3.0, withBorderColor:  UIColor.gray)
+        Utilities.setTextFieldCornerRadius(forTextField: txtJobTitle, withRadius: 3.0, withBorderColor:  UIColor.gray)
+        
+        Utilities.setButtonCornerRadius(button: btnJobType, withRadius: 3.0, withBorderColor: UIColor.gray)
+        
+        txtYearOfGraduation.setLeftPaddingPoints(10)
+        txtJobTitle.setLeftPaddingPoints(10)
+        
+        
+        btnJobType.imageEdgeInsets = UIEdgeInsetsMake(0, btnJobType.frame.size.width - 40, 0, 0)
+        btnJobType.titleEdgeInsets = UIEdgeInsetsMake(0, -20, 0, 20)
+        
+        txtAboutJob.layer.cornerRadius = 3.0
+        txtAboutJob.layer.borderWidth = 1.0
+        txtAboutJob.layer.borderColor = UIColor.gray.cgColor
+        
+        btnUploadVideo.layer.cornerRadius = 3.0
+        btnSubmit.layer.cornerRadius = 3.0
+        
     }
     
     // MARK: - TextField Delegate Methods
@@ -79,7 +88,19 @@ class CandidateFilterViewController: UIViewController,UITextFieldDelegate {
         return false
     }
     
+    func textViewShouldReturn(textView: UITextView!) -> Bool {
+        
+        textView.resignFirstResponder()
+        return false;
+    }
     
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if (text == "\n") {
+            textView.resignFirstResponder()
+            return false
+        }
+        return true
+    }
     func registerForKeyboardNotifications(){
         //Adding notifies on keyboard appearing
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWasShown(notification:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
@@ -109,6 +130,13 @@ class CandidateFilterViewController: UIViewController,UITextFieldDelegate {
                 self.scrollView.scrollRectToVisible(activeField.frame, animated: true)
             }
         }
+        
+        if let activeTextView = self.activeTextView {
+            if (!aRect.contains(activeTextView.frame.origin)){
+                self.scrollView.scrollRectToVisible(activeTextView.frame, animated: true)
+            }
+        }
+        
     }
     
     func keyboardWillBeHidden(notification: NSNotification){
@@ -119,19 +147,27 @@ class CandidateFilterViewController: UIViewController,UITextFieldDelegate {
         
     }
     func textFieldDidBeginEditing(_ textField: UITextField){
-         Utilities.setTextFieldCornerRadius(forTextField: textField, withRadius: 3.0, withBorderColor: UIColor.blue)
-        // Utilities.setTextFieldBorderBelow(forTextField: textField,color: UIColor.blue)
-        
-        
+        Utilities.setTextFieldCornerRadius(forTextField: textField, withRadius: 3.0, withBorderColor: UIColor.blue)
         activeField = textField
     }
     
     func textFieldDidEndEditing(_ textField: UITextField){
         Utilities.setTextFieldCornerRadius(forTextField: textField, withRadius: 3.0, withBorderColor: UIColor.gray)
-        //Utilities.setTextFieldBorderBelow(forTextField: textField,color: UIColor.gray)
         activeField = nil
     }
     
+    func textViewShouldBeginEditing(_ textView: UITextView) -> Bool {
+        Utilities.setTextViewCornerRadius(forTextView: textView, withRadius: 3.0, withBorderColor: UIColor.blue)
+        activeTextView = textView
+        return true
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+         Utilities.setTextViewCornerRadius(forTextView: textView, withRadius: 3.0, withBorderColor: UIColor.gray)
+        activeTextView = nil
+    }
+
+
    
     /*
     // MARK: - Navigation
